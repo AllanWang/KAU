971738ae5942c3439568c54393708a3dd63c87f6
Allan Wang <me@allanwang.ca>
2017-06-22 19:23:29 -0700

    Change color lighten and darken

--- library/src/main/kotlin/ca/allanwang/kau/utils/ColorUtils.kt
+++ library/src/main/kotlin/ca/allanwang/kau/utils/ColorUtils.kt
@@ -71,14 +71,14 @@ fun Int.withMinAlpha(@IntRange(from = 0L, to = 255L) alpha: Int): Int
 @ColorInt
 fun Int.lighten(@FloatRange(from = 0.0, to = 1.0) factor: Float = 0.2f): Int {
     val (red, green, blue) = intArrayOf(Color.red(this), Color.green(this), Color.blue(this))
-            .map { Math.min(it * (1 + factor), 255.0f).toInt() }
+            .map { Math.min(255f * factor + it, 255f).toInt() }
     return Color.argb(Color.alpha(this), red, green, blue)
 }
 
 @ColorInt
 fun Int.darken(@FloatRange(from = 0.0, to = 1.0) factor: Float = 0.2f): Int {
     val (red, green, blue) = intArrayOf(Color.red(this), Color.green(this), Color.blue(this))
-            .map { Math.max(it * (1 - factor), 0f).toInt() }
+            .map { Math.max(it - 255f * factor, 0f).toInt() }
     return Color.argb(Color.alpha(this), red, green, blue)
 }
 
