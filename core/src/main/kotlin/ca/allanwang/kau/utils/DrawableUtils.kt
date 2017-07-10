aaf5b1bb81f7b81a6d28bb28b5fa6de49f2aa3ca
Allan Wang <me@allanwang.ca>
2017-07-09 22:31:13 -0700

    Clean up DrawableUtils

--- core/src/main/kotlin/ca/allanwang/kau/utils/DrawableUtils.kt
+++ core/src/main/kotlin/ca/allanwang/kau/utils/DrawableUtils.kt
@@ -1,20 +1,20 @@
 package ca.allanwang.kau.utils
 
 import android.content.res.ColorStateList
-import android.graphics.PorterDuff
 import android.graphics.drawable.Drawable
 import android.support.annotation.ColorInt
 import android.support.v4.graphics.drawable.DrawableCompat
 
-fun Drawable.tintWithColor(@ColorInt color:Int):Drawable? {
-    val nDrawable = DrawableCompat.wrap(mutate())
-    DrawableCompat.setTintMode(nDrawable, PorterDuff.Mode.SRC_IN)
-    DrawableCompat.setTint(nDrawable, color)
-    return nDrawable
-}
+/**
+ * Wrap the color into a state and tint the drawable
+ */
+fun Drawable.tint(@ColorInt color: Int): Drawable = tint(ColorStateList.valueOf(color))
 
-fun Drawable.tintWithColor(sl:ColorStateList):Drawable? {
-    val nDrawable = DrawableCompat.wrap(mutate())
-    DrawableCompat.setTintList(nDrawable, sl)
-    return nDrawable
+/**
+ * Tint the drawable with a given color state list
+ */
+fun Drawable.tint(state: ColorStateList): Drawable {
+    val drawable = DrawableCompat.wrap(mutate())
+    DrawableCompat.setTintList(drawable, state)
+    return drawable
 }
