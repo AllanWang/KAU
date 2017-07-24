b419bd1a911a28ef41abfff5b5859c37c4b80f0d
Allan Wang <me@allanwang.ca>
2017-07-23 23:38:31 -0700

    Use kotlin color for sample

--- sample/src/main/kotlin/ca/allanwang/kau/sample/KPrefSample.kt
+++ sample/src/main/kotlin/ca/allanwang/kau/sample/KPrefSample.kt
@@ -9,7 +9,7 @@ import ca.allanwang.kau.kpref.kpref
  */
 object KPrefSample : KPref() {
     var textColor: Int by kpref("TEXT_COLOR", Color.WHITE)
-    var accentColor: Int by kpref("ACCENT_COLOR", 0xffff4081.toInt())
+    var accentColor: Int by kpref("ACCENT_COLOR", 0xffff8900.toInt())
     var bgColor: Int by kpref("BG_COLOR", 0xff303030.toInt())
     var check1: Boolean by kpref("check1", true)
     var check2: Boolean by kpref("check2", false)
