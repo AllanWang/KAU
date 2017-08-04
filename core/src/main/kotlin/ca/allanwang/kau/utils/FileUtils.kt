08a3ca04f581d2f77d9791b8e1536be2d299b08e
Allan Wang <me@allanwang.ca>
2017-08-04 15:24:47 -0700

    Create FileUtils and NotificationUtils

---
+++ core/src/main/kotlin/ca/allanwang/kau/utils/FileUtils.kt
@@ -0,0 +1,33 @@
+package ca.allanwang.kau.utils
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.os.Environment
+import java.io.File
+import java.io.IOException
+import java.io.InputStream
+import java.text.SimpleDateFormat
+
+/**
+ * Created by Allan Wang on 2017-08-04.
+ */
+@Throws(IOException::class)
+fun createMediaFile(prefix: String, extension: String): File {
+    val timeStamp = SimpleDateFormat.getDateInstance().parse("yyyyMMdd_HHmmss")
+    val imageFileName = "${prefix}_${timeStamp}_"
+    val storageDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES)
+    val frostDir = File(storageDir, "Frost")
+    if (!frostDir.exists()) frostDir.mkdirs()
+    return File.createTempFile(imageFileName, extension, frostDir)
+}
+
+@Throws
+fun Context.createPrivateMediaFile(prefix: String, extension: String): File {
+    val timeStamp = SimpleDateFormat.getDateInstance().parse("yyyyMMdd_HHmmss")
+    val imageFileName = "${prefix}_${timeStamp}_"
+    val storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+    return File.createTempFile(imageFileName, extension, storageDir)
+}
+
+fun File.copyFromInputStream(inputStream: InputStream)
+        = inputStream.use { input -> outputStream().use { output -> input.copyTo(output) } }
