3b86446b3debfbf05ea08fc78d78d0b548409631
Allan Wang <me@allanwang.ca>
2017-08-06 11:40:50 -0700

    Finish zips with tests

---
+++ core/src/main/kotlin/ca/allanwang/kau/kotlin/Zip.kt
@@ -0,0 +1,90 @@
+package ca.allanwang.kau.kotlin
+
+import org.jetbrains.anko.doAsync
+import java.util.concurrent.atomic.AtomicInteger
+
+/**
+ * Created by Allan Wang on 2017-08-06.
+ *
+ * Collection of zip methods that aim to replicate
+ * <a href="http://reactivex.io/documentation/operators/zip.html">Reactive Zips</a>
+ * For unit returning functions
+ *
+ * Typically, the functions will execute asynchronously and call their given callbacks when finished.
+ * Once all callbacks are called, the final onFinish callback will be executed.
+ *
+ * There is also a helper zipper to wrap synchronous functions with Anko's doAsync to achieve the same results
+ *
+ * Note that not wrapping synchronous functions will render these methods useless,
+ * as you can simply define an inline callback after all functions are finished
+ */
+
+/**
+ * Callback which will only execute the first time
+ */
+open class ZipCallbackBase {
+    var completed: Boolean = false
+
+    inline operator fun invoke(callback: () -> Unit) {
+        if (completed) return
+        completed = true
+        callback()
+    }
+}
+
+class ZipCallback<T>(val onReceived: (T) -> Unit) : ZipCallbackBase() {
+    operator fun invoke(result: T) = invoke { onReceived(result) }
+}
+
+class ZipEmptyCallback(val onReceived: () -> Unit) : ZipCallbackBase() {
+    operator fun invoke() = invoke(onReceived)
+}
+
+/**
+ * Given a default result, a series of tasks, and a finished callback,
+ * this method will run all tasks and wait until all tasks emit a response
+ * The response will then be sent back to the callback
+ *
+ * ALl tasks must invoke the task callback for [onFinished] to execute
+ */
+inline fun <reified T> Collection<(ZipCallback<T>) -> Unit>.zip(
+        defaultResult: T, crossinline onFinished: (results: Array<T>) -> Unit
+) {
+    val result = Array(size) { defaultResult }
+    val countDown = AtomicInteger(size)
+    forEachIndexed { index, asyncFun ->
+        asyncFun(ZipCallback<T> {
+            result[index] = it
+            if (countDown.decrementAndGet() <= 0)
+                onFinished(result)
+        })
+    }
+}
+
+/**
+ * Simplified zip method with no finished callback arguments
+ */
+inline fun Collection<(ZipEmptyCallback) -> Unit>.zip(crossinline onFinished: () -> Unit) {
+    val countDown = AtomicInteger(size)
+    forEach { asyncFun ->
+        asyncFun(ZipEmptyCallback {
+            if (countDown.decrementAndGet() <= 0)
+                onFinished()
+        })
+    }
+}
+
+/**
+ * Converts a collection of synchronous tasks to asynchronous tasks with a common callback
+ */
+inline fun Collection<() -> Unit>.zipAsync(crossinline onFinished: () -> Unit) {
+    map { synchronousFun ->
+        {
+            callback: ZipEmptyCallback ->
+            doAsync {
+                synchronousFun()
+                callback()
+            }; Unit
+        }
+    }.zip(onFinished)
+}
---
+++ core/src/test/kotlin/ca/allanwang/kau/kotlin/ZipTest.kt
@@ -0,0 +1,74 @@
+package ca.allanwang.kau.kotlin
+
+import org.jetbrains.anko.doAsync
+import org.junit.Test
+import java.util.*
+import java.util.concurrent.CountDownLatch
+import java.util.concurrent.TimeUnit
+import kotlin.test.assertEquals
+import kotlin.test.assertTrue
+import kotlin.test.fail
+
+/**
+ * Created by Allan Wang on 2017-08-06.
+ */
+class ZipTest {
+
+    val debug = false
+
+    fun p(text: String) {
+        if (debug) println(text)
+    }
+
+    @Test
+    fun basic() {
+        val start = System.currentTimeMillis()
+        val latch = CountDownLatch(1)
+        val rnd = Random()
+        (0..10).map {
+            {
+                callback: ZipCallback<Int> ->
+                doAsync {
+                    val sleepTime = rnd.nextInt(100) + 200L
+                    p("Task $it will sleep for ${sleepTime}ms")
+                    Thread.sleep(sleepTime)
+                    val finish = System.currentTimeMillis()
+                    p("Task $it finished in ${finish - start}ms at $finish")
+                    callback(it)
+                }; Unit
+            }
+        }.zip(-1) {
+            results ->
+            val finish = System.currentTimeMillis()
+            println("Results ${results.contentToString()} received in ${finish - start}ms at $finish")
+            assertTrue((0..10).toList().toTypedArray().contentEquals(results), "Basic zip results do not match")
+            assertTrue(finish - start < 1000L, "Basic zip does not seem to be running asynchronously")
+            latch.countDown()
+
+        }
+        latch.await(1100, TimeUnit.MILLISECONDS)
+    }
+
+    @Test
+    fun basicAsync() {
+        val start = System.currentTimeMillis()
+        val latch = CountDownLatch(1)
+        val rnd = Random()
+        (0..10).map {
+            {
+                val sleepTime = rnd.nextInt(100) + 200L
+                p("Task $it will sleep for ${sleepTime}ms")
+                Thread.sleep(sleepTime)
+                val finish = System.currentTimeMillis()
+                p("Task $it finished in ${finish - start}ms at $finish")
+            }
+        }.zipAsync {
+            val finish = System.currentTimeMillis()
+            println("Results received in ${finish - start}ms at $finish")
+            assertTrue(finish - start < 1000L, "BasicAsync does not seem to be wrapping the tasks asynchronously")
+            latch.countDown()
+        }
+        latch.await(1100, TimeUnit.MILLISECONDS)
+    }
+
+}
