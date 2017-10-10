ac9343605baa0c5969e1a9ad4cd4db0bdf7816c8
Allan Wang <me@allanwang.ca>
2017-10-10 17:22:41 -0400

    Replace multi if else with when

--- core-ui/src/main/kotlin/ca/allanwang/kau/ui/views/CutoutView.kt
+++ core-ui/src/main/kotlin/ca/allanwang/kau/ui/views/CutoutView.kt
@@ -147,10 +147,12 @@ class CutoutView @JvmOverloads constructor(
         paint.textSize = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_PX, mid, metrics)
         val maxLineWidth = paint.measureText(text)
 
-        return if (high - low < precision) low
-        else if (maxLineWidth > targetWidth) getSingleLineTextSize(text, paint, targetWidth, low, mid, precision, metrics)
-        else if (maxLineWidth < targetWidth) getSingleLineTextSize(text, paint, targetWidth, mid, high, precision, metrics)
-        else mid
+        return when {
+            high - low < precision -> low
+            maxLineWidth > targetWidth -> getSingleLineTextSize(text, paint, targetWidth, low, mid, precision, metrics)
+            maxLineWidth < targetWidth -> getSingleLineTextSize(text, paint, targetWidth, mid, high, precision, metrics)
+            else -> mid
+        }
     }
 
     private fun createBitmap() {
--- core/src/main/kotlin/ca/allanwang/kau/swipe/SwipeBackLayout.kt
+++ core/src/main/kotlin/ca/allanwang/kau/swipe/SwipeBackLayout.kt
@@ -390,15 +390,19 @@ internal class SwipeBackLayout @JvmOverloads constructor(context: Context, attrs
         }
 
         override fun clampViewPositionHorizontal(child: View, left: Int, dx: Int): Int {
-            return if (edgeFlag == SWIPE_EDGE_RIGHT) Math.min(0, Math.max(left, -child.width))
-            else if (edgeFlag == SWIPE_EDGE_LEFT) Math.min(child.width, Math.max(left, 0))
-            else 0
+            return when (edgeFlag) {
+                SWIPE_EDGE_RIGHT -> Math.min(0, Math.max(left, -child.width))
+                SWIPE_EDGE_LEFT -> Math.min(child.width, Math.max(left, 0))
+                else -> 0
+            }
         }
 
         override fun clampViewPositionVertical(child: View, top: Int, dy: Int): Int {
-            return if (edgeFlag == SWIPE_EDGE_BOTTOM) Math.min(0, Math.max(top, -child.height))
-            else if (edgeFlag == SWIPE_EDGE_TOP) Math.min(child.height, Math.max(top, 0))
-            else 0
+            return when (edgeFlag) {
+                SWIPE_EDGE_BOTTOM -> Math.min(0, Math.max(top, -child.height))
+                SWIPE_EDGE_TOP -> Math.min(child.height, Math.max(top, 0))
+                else -> 0
+            }
         }
     }
 
