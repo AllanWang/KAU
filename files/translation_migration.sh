87c8c5394638b50e561a9ffd7165f40ef29d2302
Allan Wang <me@allanwang.ca>
2017-10-24 15:40:22 -0400

    Add french translations (#94)
    * Add french translations
    
    * Add translatable tags to about

---
+++ files/translation_migration.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+BASE_MODULE=kpref-activity
+FOLDER=values-fr-rFR
+OUTPUT=values-fr
+
+cd ..
+
+current=${PWD##*/}
+
+if [ "$current" != "KAU" ]; then
+    echo "Not in KAU";
+    return;
+fi
+
+mkdir -p about/src/main/res/${OUTPUT}
+mv ${BASE_MODULE}/src/main/res/${FOLDER}/strings_about.xml about/src/main/res/${OUTPUT}/strings_about.xml
+
+mkdir -p colorpicker/src/main/res/${OUTPUT}
+mv ${BASE_MODULE}/src/main/res/${FOLDER}/strings_colorpicker.xml colorpicker/src/main/res/${OUTPUT}/strings_colorpicker.xml
+
+mkdir -p core/src/main/res-public/${OUTPUT}
+mv ${BASE_MODULE}/src/main/res/${FOLDER}/strings_commons.xml core/src/main/res-public/${OUTPUT}/strings_commons.xml
+
+mkdir -p mediapicker/src/main/res/${OUTPUT}
+mv ${BASE_MODULE}/src/main/res/${FOLDER}/strings_mediapicker.xml mediapicker/src/main/res/${OUTPUT}/strings_mediapicker.xml
