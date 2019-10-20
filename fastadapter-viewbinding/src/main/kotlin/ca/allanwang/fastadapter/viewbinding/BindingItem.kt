7c421439d69895053d90a44236059f8baf57a853
Allan Wang <me@allanwang.ca>
2019-10-20 00:10:51 -0700

    Update imports

--- fastadapter-viewbinding/src/main/kotlin/ca/allanwang/fastadapter/viewbinding/BindingItem.kt
+++ fastadapter-viewbinding/src/main/kotlin/ca/allanwang/fastadapter/viewbinding/BindingItem.kt
@@ -20,7 +20,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import androidx.viewbinding.ViewBinding
-import ca.allanwang.kau.fastadapter.databinding.R
+import ca.allanwang.kau.fastadapter.viewbinding.R
 import ca.allanwang.kau.logging.KL
 import com.mikepenz.fastadapter.FastAdapter
 import com.mikepenz.fastadapter.GenericItem
