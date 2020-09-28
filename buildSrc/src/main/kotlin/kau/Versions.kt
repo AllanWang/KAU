ce1f5b0ae72c62ec75a03bb452a2d76916ee558e
Allan Wang <me@allanwang.ca>
2020-09-27 19:51:03 -0700

    Update remaining versinos

--- buildSrc/src/main/kotlin/kau/Versions.kt
+++ buildSrc/src/main/kotlin/kau/Versions.kt
@@ -33,13 +33,13 @@ object Versions {
     const val coroutines = "1.3.9"
 
     // https://github.com/mikepenz/AboutLibraries/releases
-    const val aboutLibraries = "8.3.0"
+    const val aboutLibraries = "8.3.1"
 
     // https://github.com/wasabeef/Blurry/releases
     const val blurry = "3.0.0"
 
     // https://github.com/mikepenz/FastAdapter/releases
-    const val fastAdapter = "5.2.2"
+    const val fastAdapter = "5.2.3"
 
     // https://github.com/bumptech/glide/releases
     const val glide = "4.11.0"
@@ -48,7 +48,7 @@ object Versions {
     const val iconics = "5.0.3"
     const val iconicsGoogle = "3.0.1.4"
     const val iconicsMaterial = "2.2.0.6"
-    const val iconicsCommunity = "3.5.95.1"
+    const val iconicsCommunity = "5.3.45.1"
 
     // https://github.com/afollestad/material-dialogs/releases
     const val materialDialog = "3.3.0"
@@ -57,42 +57,42 @@ object Versions {
     const val koin = "2.1.6"
 
     // https://mvnrepository.com/artifact/com.google.dagger/hilt-android
-    const val hilt = "2.28.3-alpha"
+    const val hilt = "2.29.1-alpha"
 
     // https://square.github.io/leakcanary/changelog/
     const val leakCanary = "2.4"
 
     // https://mvnrepository.com/artifact/androidx.test.espresso/espresso-core?repo=google
-    const val espresso = "3.3.0-rc01"
+    const val espresso = "3.3.0"
 
     // https://mvnrepository.com/artifact/org.junit.jupiter/junit-jupiter-api
     const val junit = "4.13"
 
     // https://mvnrepository.com/artifact/androidx.test.ext/junit
-    const val testRunner = "1.1.2-rc01"
+    const val testRunner = "1.1.2"
 
     // https://mvnrepository.com/artifact/androidx.test/rules?repo=google
-    const val testRules = "1.3.0-rc01"
+    const val testRules = "1.3.0"
 
     // Keep version for now, see https://github.com/diffplug/spotless/issues/521
     // https://github.com/diffplug/spotless/blob/master/plugin-gradle/CHANGES.md
     const val spotless = "3.28.0"
 
     // https://github.com/bugsnag/bugsnag-android/releases
-    const val bugsnag = "4.22.3"
+    const val bugsnag = "5.2.0"
 
     // https://github.com/bugsnag/bugsnag-android-gradle-plugin/releases
-    const val bugsnagPlugin="4.7.5"
+    const val bugsnagPlugin="5.2.0"
 
     // https://mvnrepository.com/artifact/com.android.tools.build/gradle?repo=google
     const val gradlePlugin = "4.1.0-rc03"
     // https://github.com/dcendents/android-maven-gradle-plugin/releases
     const val mavenPlugin = "2.1"
     // https://github.com/Triple-T/gradle-play-publisher/releases
-    const val playPublishPlugin = "2.8.0"
+    const val playPublishPlugin = "3.0.0"
 
     // https://github.com/KeepSafe/dexcount-gradle-plugin/releases
-    const val dexCountPlugin = "1.0.3"
+    const val dexCountPlugin = "2.0.0-RC1"
 
     // https://github.com/gladed/gradle-android-git-version/releases
     const val gitVersionPlugin = "0.4.13"
