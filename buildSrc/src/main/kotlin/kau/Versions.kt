ddf274f4f04e1f3457c88c2186dbf02a3fefb699
Allan Wang <me@allanwang.ca>
2020-09-27 19:15:30 -0700

    Update android plugin

--- buildSrc/src/main/kotlin/kau/Versions.kt
+++ buildSrc/src/main/kotlin/kau/Versions.kt
@@ -85,7 +85,7 @@ object Versions {
     const val bugsnagPlugin="4.7.5"
 
     // https://mvnrepository.com/artifact/com.android.tools.build/gradle?repo=google
-    const val gradlePlugin = "4.1.0-beta04"
+    const val gradlePlugin = "4.1.0-rc03"
     // https://github.com/dcendents/android-maven-gradle-plugin/releases
     const val mavenPlugin = "2.1"
     // https://github.com/Triple-T/gradle-play-publisher/releases
