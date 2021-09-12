e15fb268ab18aa4a6b1b8cb3afbfa6b895433fb5
Allan Wang <me@allanwang.ca>
2021-09-11 19:39:19 -0700

    Test build release

--- files/github_actions.sh
+++ files/github_actions.sh
@@ -4,8 +4,6 @@
 # https://docs.github.com/en/actions/reference/encrypted-secrets#limits-for-secrets
 
 rm kau_github.tar.gpg
-cd ..
-tar cvf kau_github.tar files/gplay-keys.json files/kau.keystore files/kau.properties
+tar cvf kau_github.tar gplay-keys.json kau.keystore kau.properties
 gpg --symmetric --cipher-algo AES256 kau_github.tar
 rm kau_github.tar
-mv kau_github.tar.gpg files/
